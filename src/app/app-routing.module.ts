6cdaa40554ac81b82c00cb303b4e6ffda57e0950
Itamar <iherreraf.12@gmail.com>
2024-05-31 18:10:22 -0500

    Eliminación de archivos, y agregue nuevos componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ClienteComponent } from './pages/diagnosticoproyecto/cliente/cliente.component';
-import { CuadroNecesidadesComponent } from './pages/diagnosticoproyecto/cuadro-necesidades/cuadro-necesidades.component';
-import { DatosAdicionalesComponent } from './pages/diagnosticoproyecto/datos-adicionales/datos-adicionales.component';
-import { RecomendacionesComponent } from './pages/diagnosticoproyecto/recomendaciones/recomendaciones.component';
-import { PrimerNivelComponent } from './pages/diagnosticoproyecto/cliente/primer-nivel/primer-nivel.component';
-import { SegundoNivelComponent } from './pages/diagnosticoproyecto/cliente/segundo-nivel/segundo-nivel.component';
-import { TercerNivelComponent } from './pages/diagnosticoproyecto/cliente/tercer-nivel/tercer-nivel.component';
 import { DiagnosticoProyectoComponent } from './pages/diagnosticoproyecto/diagnostico-proyecto.component';
+import { AgradecimientoComponent } from './pages/agradecimiento/agradecimiento.component';
 
 const routes: Routes = [
   {path: 'diagnostico', component: DiagnosticoProyectoComponent},
-  { path: 'clientes', component: ClienteComponent },
-  { path: 'primer-nivel', component: PrimerNivelComponent},
-  { path: 'segundo-nivel', component: SegundoNivelComponent},
-  { path: 'tercer-nivel', component: TercerNivelComponent},
-  { path: 'cuadro-necesidades', component: CuadroNecesidadesComponent },
-  { path: 'datos-adicionales', component: DatosAdicionalesComponent },
+  {path: 'diagnostico', component: AgradecimientoComponent},
   { path: '**', redirectTo: 'diagnostico', pathMatch: 'full'}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { DatosAdicionalesComponent } from './pages/diagnosticoproyecto/datos-adicionales/datos-adicionales.component';
-import { ClienteComponent } from './pages/diagnosticoproyecto/cliente/cliente.component';
-import { CuadroNecesidadesComponent } from './pages/diagnosticoproyecto/cuadro-necesidades/cuadro-necesidades.component';
-import { RecomendacionesComponent } from './pages/diagnosticoproyecto/recomendaciones/recomendaciones.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { PrimerNivelComponent } from './pages/diagnosticoproyecto/cliente/primer-nivel/primer-nivel.component';
-import { SegundoNivelComponent } from './pages/diagnosticoproyecto/cliente/segundo-nivel/segundo-nivel.component';
-import { TercerNivelComponent } from './pages/diagnosticoproyecto/cliente/tercer-nivel/tercer-nivel.component';
 import { DiagnosticoProyectoComponent } from './pages/diagnosticoproyecto/diagnostico-proyecto.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AgradecimientoComponent } from './pages/agradecimiento/agradecimiento.component';
 @NgModule({
   declarations: [
     AppComponent,
-    ClienteComponent,
-    CuadroNecesidadesComponent,
-    RecomendacionesComponent,
-    DatosAdicionalesComponent,
-    PrimerNivelComponent,
-    SegundoNivelComponent,
-    TercerNivelComponent,
-    DiagnosticoProyectoComponent
+    DiagnosticoProyectoComponent,
+    AgradecimientoComponent
   ],
   imports: [
     BrowserModule,
